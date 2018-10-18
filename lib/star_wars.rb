fb51f9081fe61182abd9ba6d4b9ebd43d10899fb
github username <github email address>
2018-10-18 10:15:22 -0700

    Initial commit

---
+++ lib/star_wars.rb
@@ -0,0 +1,5 @@
+require "star_wars/version"
+
+module StarWars
+  # Your code goes here...
+end
---
+++ spec/star_wars_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe StarWars do
+  it "has a version number" do
+    expect(StarWars::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
