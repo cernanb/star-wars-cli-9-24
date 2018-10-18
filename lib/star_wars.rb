76e251e6234bb210fe510628b3c2bd0fbe655b79
github username <github email address>
2018-10-18 10:15:30 -0700

    Built CLI

--- lib/star_wars.rb
+++ lib/star_wars.rb
@@ -1,5 +1,16 @@
-require "star_wars/version"
+require "pry"
+require "rest-client"
+require "json"
+
+
 
 module StarWars
   # Your code goes here...
 end
+
+require_relative "./star_wars/version"
+require_relative "./star_wars/api_service"
+require_relative "./star_wars/cli"
+require_relative "./star_wars/category"
+require_relative "./star_wars/people"
+require_relative "./star_wars/planet"
